07eec8ec3a8931879801eea1bc1429e85e3e1249
ilya.basyrov <ilya.basyrov@redsolution.ru>
2023-06-02 09:52:43 +0500

    create symlink func fixed

--- gallery_new/api/utils/simlinks.py
+++ gallery_new/api/utils/simlinks.py
@@ -4,8 +4,8 @@ from django.conf import settings
 
 def create_symlink(path: str, file_name: str, title: str) -> None:
 
-    symlink_dir = str(Path(settings.MEDIA_ROOT, settings.SYMLINKS_DIR, title))
+    symlink_dir = Path(settings.MEDIA_ROOT, settings.SYMLINKS_DIR, title)
     if not symlink_dir.exists():
         symlink_dir.mkdir(parents=True)
-    src_path = str(Path(path))
+    src_path = Path(path)
     Path(symlink_dir, file_name).symlink_to(src_path)
